97ae03a56034d3db042ddfd94a0bb13abe9be384
Ignacio Sanchez <sanchezih@gmail.com>
2024-07-11 20:00:05 -0300

    update

---
+++ src/main/java/solid/_05_dip/nok/src/Client.java
@@ -0,0 +1,9 @@
+package solid._05_dip.nok.src;
+
+//Client code
+public class Client {
+	public static void main(String[] args) {
+		BusinessLogic businessLogic = new BusinessLogic();
+		businessLogic.performBusinessLogic();
+	}
+}
---
+++ src/main/java/solid/_05_dip/ok/src/Connection.java
@@ -0,0 +1,8 @@
+package solid._05_dip.ok.src;
+
+//Abstraction representing a connection interface
+public interface Connection {
+	void connect();
+
+	void disconnect();
+}
